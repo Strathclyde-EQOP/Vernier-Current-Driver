ce9419c1c907ca39fe12a14b780a062f70b6a217
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-03-29 14:34:19 +0100

    Implement CoilDriver::begin method

--- DAC8814_Driver_Latest_no_cal/CoilDriver.cpp
+++ DAC8814_Driver_Latest_no_cal/CoilDriver.cpp
@@ -1,1 +1,23 @@
 #include "CoilDriver.h"
+
+void CoilDriver::begin(uint8_t cs, uint8_t ldac, uint8_t reset, uint8_t msb){
+  pin_cs = cs;
+  pin_ldac = ldac;
+  pin_reset = reset;
+  pin_msb = msb;
+
+  digitalWrite(pin_cs, HIGH);
+  pinMode(pin_cs, OUTPUT);
+
+  digitalWrite(pin_ldac, LOW); //Disable LDAC - transparent DAC registers
+  pinMode(pin_ldac, OUTPUT);
+
+  digitalWrite(pin_reset, HIGH);
+  pinMode(pin_reset, OUTPUT);
+
+  digitalWrite(pin_msb, HIGH);
+  pinMode(pin_msb, OUTPUT);
+
+  SPI.begin();
+  SPI.beginTransaction (SPISettings (2000000, MSBFIRST, SPI_MODE0));
+}
