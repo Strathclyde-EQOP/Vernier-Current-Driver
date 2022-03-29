7f9a5ced30e5039dd1f407dfa5efece38a589cd3
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-03-29 14:13:48 +0100

    Define CoilDriver class

--- DAC8814_Driver_Latest_no_cal/CoilDriver.h
+++ DAC8814_Driver_Latest_no_cal/CoilDriver.h
@@ -1,4 +1,20 @@
 #ifndef CoilDriver_h
 #define CoilDriver_h
 
+#include <SPI.h>
+
+class CoilDriver
+{
+  public:
+    void begin(uint8_t cs, uint8_t ldac, uint8_t reset, uint8_t msb);
+    void set_dac(uint8_t channel, uint16_t code);
+    void reset();
+    
+  private:
+    int pin_cs;
+    int pin_ldac;
+    int pin_reset;
+    int pin_msb;
+};
+
 #endif
