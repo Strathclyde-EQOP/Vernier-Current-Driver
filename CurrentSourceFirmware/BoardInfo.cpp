91818899f468893c59ca31ba075efe27c31c650e
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-04-04 16:25:10 +0100

    Add basic definitions for BoardInfo methods

--- CurrentSourceFirmware/BoardInfo.cpp
+++ CurrentSourceFirmware/BoardInfo.cpp
@@ -1,1 +1,37 @@
 #include "BoardInfo.h"
+
+BoardInfo::BoardInfo(int eeprom_base_address, char *software_version):
+  kEepromBaseAddress(eeprom_base_address)
+{
+
+}
+
+
+int BoardInfo::GetMaxCurrent(char *max_current_uA) {
+  return 0;
+}
+
+
+int BoardInfo::SetMaxCurrent(uint32_t max_current_uA) {
+  return 0;
+}
+
+
+int BoardInfo::GetHardwareVersion(char *version) {
+  return 0;
+}
+
+
+int BoardInfo::SetHardwareVersion(char *version, uint8_t length) {
+  return 0;
+}
+
+
+int BoardInfo::GetBoardID(char *id) {
+  return 0;
+}
+
+
+int BoardInfo::SetBoardID(char *id, uint8_t length) {
+  return 0;
+}
