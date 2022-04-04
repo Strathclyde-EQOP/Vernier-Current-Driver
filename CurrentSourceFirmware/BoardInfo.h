8737657bf7f6813a1945009c2c8ae6bbcbae591e
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-04-04 15:44:09 +0100

    Define interface to BoardInfo class

--- CurrentSourceFirmware/BoardInfo.h
+++ CurrentSourceFirmware/BoardInfo.h
@@ -5,7 +5,26 @@
 
 
 class BoardInfo {
+  public:
+    static const uint8_t kMaxStringLength = 16;
+    struct Info {
+      uint32_t max_current_uA;
+      char software_version[kMaxStringLength];
+      char hardware_version[kMaxStringLength];
+      char board_id[kMaxStringLength];
+    };
 
+    BoardInfo(int eeprom_base_address, char *software_version);
+    int GetMaxCurrent(char *max_current_uA);
+    int SetMaxCurrent(uint32_t max_current_uA);
+    int GetHardwareVersion(char *version);
+    int SetHardwareVersion(char *version, uint8_t length);
+    int GetBoardID(char *id);
+    int SetBoardID(char *id, uint8_t length);
+
+  private:
+    const int kEepromBaseAddress;
+    Info board_info;
 };
 
 #endif
