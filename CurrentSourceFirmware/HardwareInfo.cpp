63fa3af53d2a385ed366751705849036c0188e6d
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-04-05 11:18:04 +0100

    Refactor to use GetMaxCurrentEeprom

--- CurrentSourceFirmware/HardwareInfo.cpp
+++ CurrentSourceFirmware/HardwareInfo.cpp
@@ -6,15 +6,7 @@
 
 HardwareInfo::HardwareInfo(int eeprom_base_address):
   kEepromBaseAddress(eeprom_base_address) {
-  int32_t max_current_nA;
-  int base = kEepromBaseAddress + offsetof(Info, max_current_nA);
-  EEPROM.get(base, max_current_nA);
-  if (max_current_nA >= 0) {
-    info.max_current_nA = max_current_nA;
-  }
-  else {
-    info.max_current_nA = -1;
-  }
+  info.max_current_nA = GetMaxCurrentEeprom();
 }
 
 
