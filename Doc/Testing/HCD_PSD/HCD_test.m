6b0d4b28b4fdd8c816d2b97d5c094b20d1a8fada
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-05-18 15:33:31 +0100

    Correct signal details
    Plus some minor plotting tweaks,

--- Doc/Testing/HCD_PSD/HCD_test.m
+++ Doc/Testing/HCD_PSD/HCD_test.m
@@ -1,29 +1,29 @@
-% Load test results
+% signal details
+Fs = 10e3;
+
+% signals
+load('HCD_250mA_CH1.mat');
 load('HCD_250mA_CH2.mat');
+load('HCD_250mA_CH3.mat');
 load('shorted_probe.mat');
 
-% Signal details
-N = 62500;
-T = 0.0016;
-Fs = 1/T;
-
-
+N = length(HCD_250mA_CH1);
 
 % 250 mA probe
 figure
 [X, f, C] = lpsd(HCD_250mA_CH2,@hann,Fs/N,200,2048,256,8,Fs,0.5);
 result = sqrt(X.*C.PSD);
-loglog(f, result, 'color', [1 0.1 0.1]);
-Z_10mA_10hz = result(10);
+loglog(f, result, 'color', [1 0.1 0.1], 'linewidth', 0.75);
 hold on
  
 % Noise floor
 [X, f, C] = lpsd(shorted_probe,@hann,Fs/N,200,2048,256,8,Fs,0.5);
 result = sqrt(X.*C.PSD);
-loglog(f, result, 'color', [0.3 0.3 0.3]);
+loglog(f, result, 'color', [0.3 0.3 0.3], 'linewidth', 0.75);
+hold on
 
 xlabel('Frequency (Hz)','Interpreter','tex')
 ylabel('A/\surd(Hz)','Interpreter','tex')
 grid
-set(gca,'FontSize',16)
+set(gca,'FontSize',13)
 xlim([0.1 200])
