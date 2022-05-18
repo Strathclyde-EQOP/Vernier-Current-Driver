26e1cf3e94541b72cb516d1153e3b5eca62a89ed
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-05-18 17:37:30 +0100

    Refactor to use get_spectrum helper

--- Doc/Testing/NoiseTesting/analyse_noise.m
+++ Doc/Testing/NoiseTesting/analyse_noise.m
@@ -1,20 +1,18 @@
 load('noise_data.mat');
 
+%% Shorted Probe
+[nasd_probe, f_probe] = get_spectrum(shorted_probe, Fs);
+
 %% HCD
-N = length(HCD_250mA);
 
 % 250 mA probe
 figure
-[X, f, C] = lpsd(HCD_250mA(:,3),@hann,Fs/N,200,2048,256,8,Fs,0.5);
-result = sqrt(X.*C.PSD);
-loglog(f, result, 'color', [1 0.1 0.1], 'linewidth', 0.75);
+[nasd_HCD_250mA_CH3, f_HCD_250mA_CH3] = get_spectrum(HCD_250mA(:,3), Fs);
+loglog(f_HCD_250mA_CH3, nasd_HCD_250mA_CH3, 'color', [1 0.1 0.1], 'linewidth', 0.75);
 hold on
  
 % Noise floor
-[X, f, C] = lpsd(shorted_probe,@hann,Fs/N,200,2048,256,8,Fs,0.5);
-result = sqrt(X.*C.PSD);
-loglog(f, result, 'color', [0.3 0.3 0.3], 'linewidth', 0.75);
-hold on
+loglog(f_probe, nasd_probe, 'color', [0.6 0.6 0.6], 'linewidth', 0.75);
 
 xlabel('Frequency (Hz)','Interpreter','tex')
 ylabel('A/\surd(Hz)','Interpreter','tex')
@@ -23,20 +21,18 @@ set(gca,'FontSize',13)
 xlim([0.1 200])
 
 %% LCD
-N = length(LCD_10mA);
-
 data = struct(...
-    'data', {LCD_2_5mA_CH3 LCD_10ma_output_2_5ma_CH3 LCD_10mA(:,3) shorted_probe}, ...
-    'color', {[0 0 1] [0.09,0.77,0.64] [1 0 0 ] [0.6 0.6 0.6]});
+    'data', {LCD_2_5mA_CH3 LCD_10ma_output_2_5ma_CH3 LCD_10mA(:,3)}, ...
+    'color', {[0 0 1] [0.09,0.77,0.64] [1 0 0]});
 
 fig = figure();
 hold on
 
-for this = data 
-    [X, f, C] = lpsd(this.data,@hann,Fs/N,200,2048,256,8,Fs,0.5);
-    result = sqrt(X.*C.PSD);
-    loglog(f, result, 'color', this.color, 'linewidth', 0.75)
+for this = data
+    [nasd, f] = get_spectrum(this.data, Fs);
+    loglog(f, nasd, 'color', this.color, 'linewidth', 0.75)
 end
+loglog(f_probe, nasd_probe, 'color', [0.6 0.6 0.6], 'linewidth', 0.75);
 
 xlabel('Frequency (Hz)','Interpreter','tex')
 ylabel('A/\surd(Hz)','Interpreter','tex')
@@ -46,3 +42,10 @@ ax.FontSize = 13;
 ax.XLim = [0.1 200];
 ax.XScale = "log";
 ax.YScale = "log";
+
+%% Helper Functions
+function [nasd, f] = get_spectrum(x, fs)
+    N = length(x);
+    [X, f, C] = lpsd(x, @hann, fs/N, 200, 2048, 256, 8, fs, 0.5);
+    nasd = sqrt(X.*C.PSD);
+end
