8b49b49c0d6b52e5154bd9cb9ff1cda4dfd26b55
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-05-18 17:11:44 +0100

    Move noise analysis to common script

--- Doc/Testing/NoiseTesting/analyse_noise.m
+++ Doc/Testing/NoiseTesting/analyse_noise.m
@@ -1,4 +1,28 @@
 load('noise_data.mat');
+
+%% HCD
+N = length(HCD_250mA);
+
+% 250 mA probe
+figure
+[X, f, C] = lpsd(HCD_250mA(:,3),@hann,Fs/N,200,2048,256,8,Fs,0.5);
+result = sqrt(X.*C.PSD);
+loglog(f, result, 'color', [1 0.1 0.1], 'linewidth', 0.75);
+hold on
+ 
+% Noise floor
+[X, f, C] = lpsd(shorted_probe,@hann,Fs/N,200,2048,256,8,Fs,0.5);
+result = sqrt(X.*C.PSD);
+loglog(f, result, 'color', [0.3 0.3 0.3], 'linewidth', 0.75);
+hold on
+
+xlabel('Frequency (Hz)','Interpreter','tex')
+ylabel('A/\surd(Hz)','Interpreter','tex')
+grid
+set(gca,'FontSize',13)
+xlim([0.1 200])
+
+%% LCD
 N = length(LCD_10mA);
 
 data = struct(...
@@ -22,21 +46,3 @@ ax.FontSize = 13;
 ax.XLim = [0.1 200];
 ax.XScale = "log";
 ax.YScale = "log";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
