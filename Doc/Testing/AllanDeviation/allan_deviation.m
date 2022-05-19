62de865a9e8c9dfd6e4f29c069659e504dc0a310
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-05-19 13:13:08 +0100

    Fix x limit to actually be a day

--- Doc/Testing/AllanDeviation/allan_deviation.m
+++ Doc/Testing/AllanDeviation/allan_deviation.m
@@ -1,20 +1,20 @@
 load("stability_data.mat")
 
 figure()
-[avar_lcd,tau_lcd] = allanvar(LCD_10mA_stability,'octave',Fs);
+[avar_lcd,tau_lcd] = allanvar(LCD_10mA_stability, 'octave', Fs);
 adev_lcd = sqrt(avar_lcd);
 adev_lcd_ppm = adev_lcd / abs(mean(LCD_10mA_stability)) * 1e6;
-loglog(tau_lcd,adev_lcd_ppm, 'color', [0 0 1],'LineWidth',0.75);
+loglog(tau_lcd, adev_lcd_ppm, 'color', [0 0 1], 'LineWidth', 0.75);
 hold on;
 
-[avar_hcd,tau_hcd] = allanvar(HCD_250mA_stability,'octave',Fs); 
+[avar_hcd,tau_hcd] = allanvar(HCD_250mA_stability, 'octave', Fs); 
 adev_hcd = sqrt(avar_hcd);
 adev_hcd_ppm = adev_hcd / abs(mean(HCD_250mA_stability)) * 1e6;                                     
-loglog(tau_hcd,adev_hcd_ppm, 'color', [1 0 0],'LineWidth',0.75);
+loglog(tau_hcd, adev_hcd_ppm, 'color', [1 0 0], 'LineWidth', 0.75);
 hold on;
 
 xlabel('\tau (s)')
 ylabel('Normalised Allan deviation (ppm)')
 set(gca,'FontSize',13)
-xlim([0.25 32768]) %Show 1 day of data 
+xlim([0.25 60*60*24]) % Show 1 day of data 
 grid on
