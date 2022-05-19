1f530ab66ed45931ea59d7280706b79311152a64
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-05-19 13:37:07 +0100

    Simplify plot colours

--- Doc/Testing/AllanDeviation/allan_deviation.m
+++ Doc/Testing/AllanDeviation/allan_deviation.m
@@ -4,13 +4,13 @@ figure()
 [avar_lcd,tau_lcd] = allanvar(LCD_10mA_stability, 'octave', Fs);
 adev_lcd = sqrt(avar_lcd);
 adev_lcd_ppm = adev_lcd / abs(mean(LCD_10mA_stability)) * 1e6;
-loglog(tau_lcd, adev_lcd_ppm, 'color', [0 0 1], 'LineWidth', 0.75);
+loglog(tau_lcd, adev_lcd_ppm, 'color', 'b', 'LineWidth', 0.75);
 hold on;
 
 [avar_hcd,tau_hcd] = allanvar(HCD_250mA_stability, 'octave', Fs); 
 adev_hcd = sqrt(avar_hcd);
 adev_hcd_ppm = adev_hcd / abs(mean(HCD_250mA_stability)) * 1e6;                                     
-loglog(tau_hcd, adev_hcd_ppm, 'color', [1 0 0], 'LineWidth', 0.75);
+loglog(tau_hcd, adev_hcd_ppm, 'color', 'r', 'LineWidth', 0.75);
 hold on;
 
 xlabel('\tau (s)')
