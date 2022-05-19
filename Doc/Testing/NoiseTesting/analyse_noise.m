bb89d2573d2c1a4c32672e7bc26cb960aefa9e4d
Iain Chalmers <iain.chalmers@strath.ac.uk>
2022-05-19 09:45:01 +0100

    Move plot formatting to helper function

--- Doc/Testing/NoiseTesting/analyse_noise.m
+++ Doc/Testing/NoiseTesting/analyse_noise.m
@@ -1,31 +1,27 @@
 load('noise_data.mat');
 
+
 %% Shorted Probe
 [nasd_probe, f_probe] = get_spectrum(shorted_probe, Fs);
 
+
 %% HCD
 
-% 250 mA probe
-figure
 [nasd_HCD_250mA_CH3, f_HCD_250mA_CH3] = get_spectrum(HCD_250mA(:,3), Fs);
-loglog(f_HCD_250mA_CH3, nasd_HCD_250mA_CH3, 'color', [1 0.1 0.1], 'linewidth', 0.75);
-hold on
- 
-% Noise floor
-loglog(f_probe, nasd_probe, 'color', [0.6 0.6 0.6], 'linewidth', 0.75);
 
-xlabel('Frequency (Hz)','Interpreter','tex')
-ylabel('A/\surd(Hz)','Interpreter','tex')
-grid
-set(gca,'FontSize',13)
-xlim([0.1 200])
+fig_hcd = figure();
+hold on
+loglog(f_HCD_250mA_CH3, nasd_HCD_250mA_CH3, 'color', [1 0.1 0.1], 'linewidth', 0.75);
+loglog(f_probe, nasd_probe, 'color', [0.6 0.6 0.6], 'linewidth', 0.75);
+format_fig(fig_hcd);
+
 
 %% LCD
 data = struct(...
     'data', {LCD_2_5mA_CH3 LCD_10ma_output_2_5ma_CH3 LCD_10mA(:,3)}, ...
     'color', {[0 0 1] [0.09,0.77,0.64] [1 0 0]});
 
-fig = figure();
+fig_lcd = figure();
 hold on
 
 for this = data
@@ -34,18 +30,23 @@ for this = data
 end
 loglog(f_probe, nasd_probe, 'color', [0.6 0.6 0.6], 'linewidth', 0.75);
 
-xlabel('Frequency (Hz)','Interpreter','tex')
-ylabel('A/\surd(Hz)','Interpreter','tex')
-grid
-ax = gca;
-ax.FontSize = 13;
-ax.XLim = [0.1 200];
-ax.XScale = "log";
-ax.YScale = "log";
+format_fig(fig_lcd);
+
 
 %% Helper Functions
 function [nasd, f] = get_spectrum(x, fs)
     N = length(x);
     [X, f, C] = lpsd(x, @hann, fs/N, 200, 2048, 256, 8, fs, 0.5);
     nasd = sqrt(X.*C.PSD);
+end
+
+function [] = format_fig(fig)
+    ax = fig.CurrentAxes;
+    xlabel(ax, 'Frequency (Hz)','Interpreter','tex')
+    ylabel(ax, 'A/\surd(Hz)','Interpreter','tex')
+    grid(ax)
+    ax.FontSize = 13;
+    ax.XLim = [0.1 200];
+    ax.XScale = "log";
+    ax.YScale = "log";
 end
